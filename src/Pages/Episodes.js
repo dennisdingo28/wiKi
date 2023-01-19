c4dcb7e8fecaeb2d7df652f4a4362927b9d3399f
Moldovan Dennis <dennismoldovan32@gmail.com>
2023-01-19 21:30:32 +0200

    all pages done

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,11 +1,51 @@
-import React from 'react'
-
+import { type } from '@testing-library/user-event/dist/type';
+import React,{useState,useEffect} from 'react'
+import Cards from "../components/cards/Cards";
+import InputGroup from '../components/filters/category/InputGroup';
 const Episodes = () => {
+  const [id,setId] = useState(1);
+  const [info,setInfo] = useState([]);
+  const [results,setResults] = useState([]);
+  const {air_date,name} = info;
+
+  const api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  useEffect(()=>{
+    (async function(){
+       const data = await fetch(api).then(res=>res.json());
+       setInfo(data);
+
+       const a = await Promise.all(
+        data.characters.map((x)=>{
+          return fetch(x).then(res=>res.json());
+        })
+       );
+       setResults(a);
+    })()
+  },[api]);
+
+
   return (
-    <div>
-      The episodes are here
+    <div className='container'>
+      <div className='row mb-4'>
+        <h1 className='text-center mb-4'>Episode : <span className='text-primary'>{name==="" ? "Unknown":name}</span></h1>
+        <h5 className='text-center'>Air Date {air_date==="" ? "Unknown":air_date}</h5>
+      </div>
+      <div className='row'>
+        <div className='col-3'>
+          <h4 className='text-center mb-4'>
+            Pick Episode
+          </h4>
+          <InputGroup name="Episode" setId={setId} total={51}/>
+        </div>
+        <div className='col-8'>
+          <div className='row'>
+              <Cards results={results}/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Episodes
+export default Episodes;
--- src/Pages/Location.js
+++ src/Pages/Location.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import Cards from "../components/cards/Cards";
+import InputGroup from '../components/filters/category/InputGroup';
 
 const Location = () => {
+  const [id,setId] = useState(1);
+  const [info,setInfo] = useState([]);
+  const [results,setResults] = useState([]);
+  const {name,type,dimension} = info;
+
+  const api = `https://rickandmortyapi.com/api/location/${id}`;
+
+  useEffect(()=>{
+    (async function(){
+       const data = await fetch(api).then(res=>res.json());
+       setInfo(data);
+
+       const a = await Promise.all(
+        data.residents.map((x)=>{
+          return fetch(x).then(res=>res.json());
+        })
+       );
+       setResults(a);
+    })()
+  },[api]);
+
+
   return (
-    <div>
-      The locations are here
+    <div className='container'>
+      <div className='row mb-4'>
+        <h1 className='text-center mb-4'>Location : <span className='text-primary'>{name==="" ? "Unknown":name}</span></h1>
+        <h5 className='text-center'>Dimension :  {dimension==="" ? "Unknown":dimension}</h5>
+        <h6 className='text-center'>Type : {type==="" ? "Unknown":type}</h6>
+
+      </div>
+      <div className='row'>
+        <div className='col-3'>
+          <h4 className='text-center mb-4'>
+            Pick Location
+          </h4>
+          <InputGroup name="Location" setId={setId} total={126}/>
+        </div>
+        <div className='col-8'>
+          <div className='row'>
+              <Cards results={results}/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Location
+export default Location;
